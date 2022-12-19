1563e7a2939710fab9a41fcd409dd9c40ee5be69
MD Ripon Islam <islam876ripon@gmail.com>
2022-12-19 11:45:27 +0600

    sending single class data using class id

--- routes/teacher.js
+++ routes/teacher.js
@@ -3,9 +3,6 @@ const router = express.Router();
 const auth = require("../middlewares/auth");
 const Class = require("../models/class");
 
-
-
-
 router.get("/get-classes", auth, async (req, res) => {
 	try {
 		const classes = await Class.find({});
@@ -14,6 +11,23 @@ router.get("/get-classes", auth, async (req, res) => {
 		);
 		res.status(200).send({
 			classes: teacherClasses,
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: "Error creating acount",
+			error,
+		});
+	}
+});
+
+router.get("/get-class/:id", async (req, res) => {
+	try {
+		const cls = await Class.findById(req.params.id);
+		console.log(cls);
+
+		res.status(200).send({
+			cls: cls,
 		});
 	} catch (err) {
 		console.log(err);
