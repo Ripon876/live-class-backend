bf036647d521c2fdb246ba6a0e3f66cd2e2f6277
MD Ripon Islam <islam876ripon@gmail.com>
2023-01-03 18:51:26 +0600

    new role added with add & remove

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 const auth = require("../middlewares/auth");
 const User = require("../models/user");
 const Class = require("../models/class");
@@ -14,7 +15,7 @@ router.get("/get-classes", async (req, res) => {
 		console.log(err);
 		res.status(500).json({
 			message: "Error creating acount",
-			error,
+			err,
 		});
 	}
 });
@@ -32,8 +33,68 @@ router.get("/get-teachers", async (req, res) => {
 	} catch (err) {
 		console.log(err);
 		res.status(500).json({
-			message: "Error creating acount",
+			message: "Error getting teachers",
 			error,
+		});
+	}
+});
+
+router.get("/get-roleplayers", async (req, res) => {
+	try {
+		getRoles(req, res, "roleplayer");
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: "Error getting roles",
+			error,
+		});
+	}
+});
+
+router.post("/add-instructor", auth, async (req, res) => {
+	try {
+		createRole(req, res, "teacher", "Instructor");
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: "Error creating account",
+			err,
+		});
+	}
+});
+
+router.post("/add-roleplayer", auth, async (req, res) => {
+	try {
+		createRole(req, res, "roleplayer", "Roleplayer");
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: "Error creating account",
+			err,
+		});
+	}
+});
+
+router.delete("/remove-roleplayer", async (req, res) => {
+	try {
+		removeRole(req, res);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: "Error deleteing account",
+			err,
+		});
+	}
+});
+
+router.delete("/remove-instructor", async (req, res) => {
+	try {
+		removeRole(req, res);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: "Error deleteing account",
+			err,
 		});
 	}
 });
@@ -77,7 +138,7 @@ router.post("/create-new-class", auth, async (req, res) => {
 		console.log(err);
 		res.status(500).json({
 			message: "Error creating class",
-			error,
+			err,
 		});
 	}
 });
@@ -94,9 +155,71 @@ router.delete("/delete-class", async (req, res) => {
 		console.log(err);
 		res.status(500).json({
 			message: "Error deleteing class",
-			error,
+			err,
 		});
 	}
 });
 
 module.exports = router;
+
+const getRoles = async (req, res, role) => {
+	const roles = await User.find({ type: role }).select([
+		"-password",
+		"-type",
+		"-__v",
+	]);
+	res.status(200).send({
+		roles,
+	});
+};
+
+const createRole = async (req, res, role, title) => {
+	await User.findOne({ email: req.body.email }).then((user) => {
+		if (!user) {
+			bcrypt
+				.hash(req.body.password, 10)
+				.then((hashedPassword) => {
+					const user = new User({
+						name: req.body.name,
+						email: req.body.email,
+						password: hashedPassword,
+						phone: req.body.phone,
+						age: req.body.age,
+						type: role,
+					});
+					user.save()
+						.then((user) => {
+							res.status(201).json({
+								message: title + " added successfully",
+								user,
+							});
+						})
+						.catch((error) => {
+							res.status(500).json({
+								message: "Error creating account",
+								error,
+							});
+						});
+				})
+				.catch((e) => {
+					res.status(500).json({
+						message: "Password was not hashed successfully",
+						e,
+					});
+				});
+		} else {
+			res.status(500).json({
+				message: "Email Exist",
+			});
+		}
+	});
+};
+
+const removeRole = async (req, res) => {
+	const id = req.body.id;
+
+	await User.findByIdAndRemove(id);
+	res.status(200).send({
+		message: "Instructor removed successfully",
+	});
+};
