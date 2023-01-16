b08a128385b1d50353974a2a29f45c46bbc90542
MD Ripon Islam <islam876ripon@gmail.com>
2023-01-17 02:37:49 +0600

    showing break timer

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 5000;
 // socket server
 const io = require("socket.io")(server, {
 	cors: {
-		// origin: "http://localhost:3000",
+		origin: "http://localhost:3000",
 		// origin: "https://live-video-class.netlify.app",
-		origin: "https://rfatutors-osler.app",
+		// origin: "https://rfatutors-osler.app",
 		methods: ["GET", "POST"],
 	},
 });
@@ -94,6 +94,11 @@ io.on("connection", (socket) => {
 		}
 	});
 
+	socket.on("breakTime", () => {
+		console.log('break time')
+		socket.broadcast.emit("breakTime");
+	});
+
 	// socket.on("markedTaken", async (index, clsId, cb) => {
 	// 	try {
 	// 		let cls = await Class.findById(clsId);
@@ -103,7 +108,7 @@ io.on("connection", (socket) => {
 	// 			cls.hasToJoin--;
 	// 			await cls.save();
 	// 			cb();
-	// 		}  
+	// 		}
 	// 	} catch (err) {
 	// 		console.log("err on gettting cls : ", err);
 	// 	}
