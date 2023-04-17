a9cf7b73975c2c7c0e859da684b6f2d7351dd239
MD Ripon Islam <islam876ripon@gmail.com>
2023-04-17 17:52:19 +0600

    allowed origin updated and marking classes ongoing and finished

--- watcher-version2.js
+++ watcher-version2.js
@@ -1,5 +1,5 @@
 const Class = require("./models/class");
-let roomId = "sdfdsj23432dfdfgfd";
+
 class Watcher_V2 {
 	constructor(exams, io, users, data, clearStates) {
 		this.exams = exams;
@@ -20,7 +20,7 @@ class Watcher_V2 {
 	}
 
 	async start() {
-		// await this.markExamsAsOngoing();
+		await this.markExamsAsOngoing();
 		if (this.tempIntervl === 0) {
 			this.io.sockets.emit("examsStarted");
 		}
@@ -135,7 +135,7 @@ class Watcher_V2 {
 	async fireExamsEnd() {
 		console.log("===== exams ended =====");
 		this.io.sockets.emit("examsEnded");
-		// await this.markExamsAsFinished();
+		await this.markExamsAsFinished();
 		this.cs();
 		return;
 	}
